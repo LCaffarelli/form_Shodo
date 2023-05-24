6703b120f7b645b876c10fb3b363dfe98ef2a6e1
clamc068 <laetitia.caffarelli@gmail.com>
2023-05-24 17:14:04 +0200

    add verification when clicking

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const title = document.getElementById("title");
 const body = document.getElementById("body");
 const button = document.getElementById("saveButton");
 
-const comment = fetch("https://jsonplaceholder.typicode.com/posts", {
+fetch("https://jsonplaceholder.typicode.com/posts", {
   method: "POST",
   body: JSON.stringify({
     title: title.value,
@@ -16,9 +16,31 @@ const comment = fetch("https://jsonplaceholder.typicode.com/posts", {
   .then((response) => response.json())
   .then((json) => console.log(json));
 
-console.log(comment);
-button.addEventListener("click", success);
+button.addEventListener("click", checking);
 
-function success() {
-  alert(`Title: ${title.value}\nBody:\n"${body.value}"`);
+function checking() {
+  checkSize;
+  checkBlank;
+}
+
+function checkSize() {
+  if (title.value.trim().length < 2) {
+    alert("Votre titre doit faire plus de 2 caractères");
+  }
+  if (title.value.trim().length > 50) {
+    alert("Votre titre doit faire moins de 50 caractères");
+  }
+  if (body.value.trim().length < 1) {
+    alert("Votre commentaire doit faire plus de 1 caractère");
+  }
+  if (body.value.trim().length > 250) {
+    alert("Votre commentaire doit faire moins de 250 caractères");
+  }
+}
+function checkBlank() {
+  if (title.value.trim() === "" || body.value.trim() === "") {
+    alert("veuillez remplir tous les champs");
+  } else {
+    alert(`Titre : ${title.value}\nCommentaire : ${body.value}`);
+  }
 }
