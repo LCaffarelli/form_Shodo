96911b4d658bda7962c313066b8fd5ae4d778859
clamc068 <laetitia.caffarelli@gmail.com>
2023-05-31 15:58:23 +0200

    add how to handle errors

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-const title = document.getElementById("title");
-const body = document.getElementById("body");
-const urlJsonPlaceholder = "https://jsonplaceholder.typicode.com/posts";
+function handleForm(event) {
+  const title = document.getElementById("title");
+  const body = document.getElementById("body");
+  const urlJsonPlaceholder = "https://jsonplaceholder.typicode.com/posts";
+  function handleForm(event) {
+    const title = document.getElementById("title");
+    const body = document.getElementById("body");
+    fetch(urlJsonPlaceholder, {
+      method: "POST",
+      body: JSON.stringify({
+        title: title.value,
+        body: body.value,
+        userId: 1,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
+        } else {
+          return response.json();
+        }
+      })
+      .then((json) =>
+        alert(`Titre : ${json.title}\nCommentaire : ${json.body}`)
+      )
+      .catch((err) => alert("Error"));
 
-fetch(urlJsonPlaceholder, {
-  method: "POST",
-  body: JSON.stringify({
-    title: title.value,
-    body: body.value,
-    userId: 1,
-  }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8",
-  },
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-
-function handleForm() {
-  alert(`Titre : ${title.value}\nCommentaire : ${body.value}`);
+    event.preventDefault();
+  }
 }
