af678099205ead5dd1c40400885be0a199609611
clamc068 <laetitia.caffarelli@gmail.com>
2023-05-31 15:59:59 +0200

    remove double const and function

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
+const urlJsonPlaceholder = "https://jsonplaceholder.typicode.com/posts";
+
 function handleForm(event) {
   const title = document.getElementById("title");
   const body = document.getElementById("body");
-  const urlJsonPlaceholder = "https://jsonplaceholder.typicode.com/posts";
-  function handleForm(event) {
-    const title = document.getElementById("title");
-    const body = document.getElementById("body");
-    fetch(urlJsonPlaceholder, {
-      method: "POST",
-      body: JSON.stringify({
-        title: title.value,
-        body: body.value,
-        userId: 1,
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => {
-        console.log(response);
-        if (response.status >= 400) {
-          throw new Error("Bad response from server");
-        } else {
-          return response.json();
-        }
-      })
-      .then((json) =>
-        alert(`Titre : ${json.title}\nCommentaire : ${json.body}`)
-      )
-      .catch((err) => alert("Error"));
 
-    event.preventDefault();
-  }
+  fetch(urlJsonPlaceholder, {
+    method: "POST",
+    body: JSON.stringify({
+      title: title.value,
+      body: body.value,
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => {
+      console.log(response);
+      if (response.status >= 400) {
+        throw new Error("Bad response from server");
+      } else {
+        return response.json();
+      }
+    })
+    .then((json) => alert(`Titre : ${json.title}\nCommentaire : ${json.body}`))
+    .catch((err) => alert("Error"));
+
+  event.preventDefault();
 }
